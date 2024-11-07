80daf6b0685e33099556c6a85d5f9b8459c0c43f
Bilol <Bilol@Noutbuk-Laziz.local>
2024-11-07 10:22:48 +0500

    Merge remote changes fot test

--- docker_service/urls.py
+++ docker_service/urls.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from services import views as service_views
-from rest_framework.authtoken import views as drf_views  # Импортируем представление для получения токена
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from rest_framework.authtoken import views as drf_views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API вашего проекта",
+      default_version='v1',
+      description="Документация API для моего проекта",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="no@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', drf_views.obtain_auth_token, name='api_token_auth'),
     path('api/', include('services.urls')),
+
+    # Маршруты для Swagger и ReDoc
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
